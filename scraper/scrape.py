e4f254bb1bc95f759fef23889cf9a026787f482a
zniesen <166646207+zniesen@users.noreply.github.com>
2025-04-29 10:50:11 -0500

    Update and rename main_functions.py to scrape.py

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -39,64 +39,18 @@ import helper_tools
 import business_information
 from selenium.webdriver.firefox.options import Options
 
-
-#%% trying to work on headers things
-# """
-# fp = webdriver.FirefoxProfile()
-# path_modify_header = 'C:/xxxxxxx/modify_headers-0.7.1.1-fx.xpi'
-# fp.add_extension(path_modify_header)
-#
-# fp.set_preference("modifyheaders.headers.count", 1)
-# fp.set_preference("modifyheaders.headers.action0", "Add")
-# fp.set_preference("modifyheaders.headers.name0", "Name_of_header") # Set here the name of the header
-# fp.set_preference("modifyheaders.headers.value0", "value_of_header") # Set here the value of the header
-# fp.set_preference("modifyheaders.headers.enabled0", True)
-# fp.set_preference("modifyheaders.config.active", True)
-# fp.set_preference("modifyheaders.config.alwaysOn", True)
-# """
-
-#Options
-#
-#
-# # Set up Firefox options
-# options = Options()
-# options.add_argument('--headless')
-#
-# # Create a new instance of the Firefox driver with selenium-wire
-# driver = webdriver.Firefox(options=options)
-#
-# # Define your custom headers
-# custom_headers = {
-#     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
-#     'Accept-Language': 'en-US,en;q=0.9',
-#     'Cache-Control': 'no-cache',
-#     'DNT': '1',
-#     'Upgrade-Insecure-Requests': '1',
-#
-#     # Add more headers as needed
-# }
-#
-# # Set the custom headers for all requests
-# driver.request_interceptor = lambda request: request.headers.update(custom_headers)
-#
-# # Your target URL
-# url = "http://example.com"  # Replace with your target URL
-# all_amenities = []
-# %%
 url = "https://www.yelp.com/"
 all_amenities = []
-#driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox(options=options)
 driver.implicitly_wait(4)
 driver.get(url)  # going to Yelp
-#%%
+
 user_agent_info = driver.find_element(By.TAG_NAME, "body").text
 # print the page content
 print(user_agent_info)
-#%%
-next_button = driver.find_element(By.CSS_SELECTOR, "html body yelp-react-root div div.biz-details-page-container-outer__09f24__pZBzx.y-css-mhg9c5 div.biz-details-page-container-inner__09f24__L9S07.y-css-mhg9c5 div.y-css-1ehjqp6 div.y-css-13kng0r main#main-content.y-css-qsv6ye div.y-css-mhg9c5 div#reviews.y-css-mhg9c5 section.y-css-15jz5c7 div.y-css-mhg9c5 div.pagination__09f24__VRjN4.y-css-1l7sbyz div.y-css-mhg9c5 div.pagination-links__09f24__bmFj8.y-css-1n5biw7 div.navigation-button-container__09f24__SvcBh.y-css-mhg9c5 span.y-css-yrt0i5 a.next-link.navigation-button__09f24__m9qRz.y-css-1kw15cs")
-def born2run(driver, all_amenities):
-    #todo add in waits for elements to be clickable
 
+next_button = driver.find_element(By.CSS_SELECTOR, "html body yelp-react-root div div.biz-details-page-container-outer__09f24__pZBzx.y-css-mhg9c5 div.biz-details-page-container-inner__09f24__L9S07.y-css-mhg9c5 div.y-css-1ehjqp6 div.y-css-13kng0r main#main-content.y-css-qsv6ye div.y-css-mhg9c5 div#reviews.y-css-mhg9c5 section.y-css-15jz5c7 div.y-css-mhg9c5 div.pagination__09f24__VRjN4.y-css-1l7sbyz div.y-css-mhg9c5 div.pagination-links__09f24__bmFj8.y-css-1n5biw7 div.navigation-button-container__09f24__SvcBh.y-css-mhg9c5 span.y-css-yrt0i5 a.next-link.navigation-button__09f24__m9qRz.y-css-1kw15cs")
+def scrape(driver, all_amenities):
     business_info = business_information.getBusinessInfo(driver)
     business_id = business_info["business_id"]
     #insert_data.insert_business(business_id, business_info['business_name'], business_info['
